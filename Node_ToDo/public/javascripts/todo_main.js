b5d2bf5039ca808e784b3117405723ea94a6f27e
No-Hyunji <kiekuy47@gmail.com>
2021-02-04 12:53:42 +0900

    final

--- Node_ToDo/public/javascripts/todo_main.js
+++ Node_ToDo/public/javascripts/todo_main.js
@@ -4,12 +4,17 @@ Jquery등 front 라이브러리, 프레임워크를 사용하지 않고
 순수 JS를 활용한 코딩 
 */
 var todo_list = [];
+// 페이지가 새로 시작되면 localStorage로부터 데이터 읽어오기
+let todo_list_store = localStorage.getItem("todo_list");
+if (todo_list_store) {
+  // localStorage에서 가져온 데이터가 있으면 JSON 형태로 변경하여
+  // todo_list에 저장하라
+  todo_list = JSON.parse(todo_list_store);
+}
+// localStorage의 todo_list 데이터가 있으면
+// 그 데이터는 todo_list 배열에 담겨 있을 것이고
+// 그렇지 않으면 배열은 비어있는 상태가 된다.
 document.addEventListener("DOMContentLoaded", function () {
-  const btn_new = document.querySelector("#btn-new");
-  btn_new.addEventListener("click", function () {
-    alert(JSON.parse(todo_data).todo);
-    localStorage.setItem("todo_data", todo_data);
-  });
   document.querySelector("#btn-save").addEventListener("click", function () {
     let todo_input = document.querySelector("input");
     todo_input = document.querySelector(
